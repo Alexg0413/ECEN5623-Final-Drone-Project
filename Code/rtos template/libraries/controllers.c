6382226f30108cf83becbd64c5348e705b37b278
Ian Mcconachie <ian.mcconachie@colorado.edu>
2026-04-24 18:12:35 -0600

    finished controller

--- Code/rtos template/libraries/controllers.c
+++ Code/rtos template/libraries/controllers.c
@@ -7,6 +7,11 @@
 
 #include <libraries/controllers.h>
 #include <libraries/PID.h>
+#include "main.h"
+#include <math.h>
+
+float attitude_commands[3] = {0};  // [roll_torque, pitch_torque, yaw_torque]
+float thrust_command       = 0.0f; // normalized throttle [0, 1]
 
 // One PID instance per control axis
 static PID_t roll_pid;
@@ -29,7 +34,7 @@ void attitude_controllers_init(void)
     pid_init(&yaw_rate_pid,    KP_YAW_RATE,    KI_YAW_RATE,    KD_YAW_RATE,    IMAX_YAW_RATE,    FCUT_HZ);
 }
 
-void attitude_controllers_update(float state_vec[], float input_vec[], int output_vec[])
+void attitude_controllers_update(float state_vec[], float input_vec[])
 {
     // Compute error for attitude outer control loop (roll, pitch) 
     // error = setpoint (pilot input) - measured state
@@ -43,24 +48,90 @@ void attitude_controllers_update(float state_vec[], float input_vec[], int outpu
     float roll_rate_err   = roll_rate_trg - state_vec[3];  
     float pitch_rate_err  = pitch_rate_trg - state_vec[4];  
     
-    float roll_rate_out   = pid_update(&roll_pid,   roll_err);
-    float pitch_rate_out  = pid_update(&pitch_pid,  pitch_err);
+    float roll_rate_out   = pid_update(&roll_rate_pid,   roll_rate_err);
+    float pitch_rate_out  = pid_update(&pitch_rate_pid,  pitch_rate_err);
     float yaw_rate_out  = pid_update(&yaw_rate_pid,    yaw_rate_err);
+
+    attitude_commands[0] = roll_rate_out;
+    attitude_commands[1] = pitch_rate_out;
+    attitude_commands[2] = yaw_rate_out;
 
 }
 
+void z_stabilize_controller_update(float state_vec[], float input_vec[])
+{
+    // input form pilot [0,1]
+    float thr_in = input_vec[0];
 
-// TODO: add global variable for roll,pitch,yaw torque (output from attitude_controllers_update)
+    // adapted form Ardupilot throttle curve:
+    //
+    // expo curve for throttle input make 50% stick = hover throttle but still maps [0,1] input to [0,1] output
+    // expo = -(thr_mid - 0.5) / 0.375, clamped to +-1
+    const float expo = -(THR_MID - 0.5f) / 0.375f;
 
-// TODO: create Z-axis controller (Not PID) to calculate thrust command from thrust input
-//   expo curve controller for pilot input to thrust output (feedforward)
+    float thr_expo = thr_in * (1.0f - expo) + expo * thr_in * thr_in * thr_in;
+
+    // angle boost 
+    // adjust the throttle command based on tilt angle to maintain vertical thrust. boost = 1/cos(tilt)
+    float cos_tilt        = fminf(cosf(state_vec[0]), cosf(state_vec[1]));
+    float boost_factor = 1.0f / cos_tilt;
+    if (boost_factor > 2.0f) boost_factor = 1.75f;
+    if (boost_factor < 1.0f) boost_factor = 1.0f;
+
+    thrust_command = thr_expo * boost_factor;
+
+    // Hard-clamp output to motor-safe range.
+    if (thrust_command > 1.0f) thrust_command = 1.0f;
+    if (thrust_command < 0.0f) thrust_command = 0.0f;
+}
 
 // TODO: add altitude hold controller (PID)
 
-// TODO: Motor mixing: map roll, pitch, yaw PID outputs to PWM commands based on frame geometry
-//   handle angle boost for thrust_out
-//   add X-frame mixer: double check geometry( not quite a true X-frame 80deg between roll arms)
-//      output_vec[0] = (int)(thrust_out + roll_out - pitch_out + yaw_out);  // FL
-//      output_vec[1] = (int)(thrust_out - roll_out - pitch_out - yaw_out);  // FR
-//      output_vec[2] = (int)(thrust_out + roll_out + pitch_out - yaw_out);  // RL
-//      output_vec[3] = (int)(thrust_out - roll_out + pitch_out + yaw_out);  // RR
+void motor_mixing_update(int armed)
+{
+    /*   Roll  right   (+): left motors (FL, RL) up, right motors (FR, RR) down
+    *    Pitch up      (+): rear motors (FL, FR) up, front motors (RL, RR) down
+    *    Yaw   CW      (+): FL & RR up (CW props), FR & RL down (CCW props)
+    *
+    *    FL [2]        FR [0]
+    *          \       /
+    *           [body]
+    *          /       \
+    *    RL [1]        RR [4]
+    */
+
+    if (armed==0) 
+    {
+        output_vec[0] = PWM_MIN_US;
+        output_vec[1] = PWM_MIN_US;
+        output_vec[2] = PWM_MIN_US;
+        output_vec[3] = PWM_MIN_US;
+        return;
+    }
+
+    // Scale thrust [0,1] -> [PWM_MIN_US, PWM_MAX_US]
+    float thr   = PWM_MIN_US + thrust_command * (float)(PWM_MAX_US - PWM_MIN_US);
+
+    // Scale attitude commands +-1 -> +- MIXER_ATT_SCALE PWM counts
+    float roll  = attitude_commands[0] * MIXER_ATT_SCALE;
+    float pitch = attitude_commands[1] * MIXER_ATT_SCALE;
+    float yaw   = attitude_commands[2] * MIXER_ATT_SCALE;
+
+    // X-frame mixing
+    float fl = thr + roll + pitch + yaw;   // front-left
+    float fr = thr - roll + pitch - yaw;   // front-right
+    float rl = thr + roll - pitch - yaw;   // rear-left
+    float rr = thr - roll - pitch + yaw;   // rear-right
+
+    // Clamp each motor to the valid PWM range
+    if (fl < PWM_MIN_US) fl = PWM_MIN_US;  if (fl > PWM_MAX_US) fl = PWM_MAX_US;
+    if (fr < PWM_MIN_US) fr = PWM_MIN_US;  if (fr > PWM_MAX_US) fr = PWM_MAX_US;
+    if (rl < PWM_MIN_US) rl = PWM_MIN_US;  if (rl > PWM_MAX_US) rl = PWM_MAX_US;
+    if (rr < PWM_MIN_US) rr = PWM_MIN_US;  if (rr > PWM_MAX_US) rr = PWM_MAX_US;
+
+    output_vec[0] = (int)fr;
+    output_vec[1] = (int)rl;
+    output_vec[2] = (int)fl;
+    output_vec[3] = (int)rr;
+}
+
